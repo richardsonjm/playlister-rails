3af49035176e90e03f54024daa409f088bb2c31d
aviflombaum <avi@flombaum.com>
2013-11-05 12:20:58 -0500

    simplecov configured for rails

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'rspec/rails'
 require 'rspec/autorun'
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start 'rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
